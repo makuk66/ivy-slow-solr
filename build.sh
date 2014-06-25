c60abc36ee8f4e8ee62a3b9bb741b6264dbfbfe2
Martijn Koster <mak-github@greenhills.co.uk>
2014-06-25 12:25:21 +0100

    add a clean

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ JAR=org.apache.ivy_2.5.0.alpha_20140618062334.jar
   echo "my.ivy.jar=$JAR" > build.properties
   rm ivy/lib/*.jar
   cp ivys/$JAR ivy/lib/
+  ant clean
   ant -v -lib ivy/lib all
 } 2>&1 | perl -pne 'use POSIX qw(strftime); print strftime "%Y-%m-%dT%H:%M:%SZ ", gmtime();'
